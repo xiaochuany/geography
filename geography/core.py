13c72b71712309b30c5d8d8d2eb5599c2151375d
xiaochuany <xiaochuan.j.yang@gmail.com>
2024-03-21 14:12:42 +0100

    combine patches into the main class

--- geography/core.py
+++ geography/core.py
@@ -27,48 +27,37 @@ class RGG:
     def adj(self):
         mask = self.distance_matrix < self.r
         return {i: [j for j,v in enumerate(row) if v and j != i] for i,row in enumerate(mask)}
-    
 
+    def n_comp(self):
+        def dfs(gr,s):
+            for v in gr[s]:
+                if v not in self.parent:
+                    self.parent[v]=s
+                    dfs(gr,v)
+                    self.topo.append(v)
+        self.parent = {}
+        self.topo=[]
+        count = 0
+        for i in range(self.n):
+            if i not in self.parent:
+                count+=1
+                self.parent[i]=None
+                dfs(self.adj,i)
+                self.topo.append(i)
+        return count
+        
+    def degree_distribution(self):
+        dgr=collections.Counter([len(v) for _,v in self.adj.items()])
+        return np.array(list(dgr.items()))
 
-# %% ../nbs/00_core.ipynb 8
-@patch
-def n_comp(self:RGG):
-    def dfs(gr,s):
-        for v in gr[s]:
-            if v not in self.parent:
-                self.parent[v]=s
-                dfs(gr,v)
-                self.topo.append(v)
-    self.parent = {}
-    self.topo=[]
-    count = 0
-    for i in range(self.n):
-        if i not in self.parent:
-            count+=1
-            self.parent[i]=None
-            dfs(self.adj,i)
-            self.topo.append(i)
-    return count
+    def cyclic(self):
+        for k, lst in self.adj.items():
+            for v in lst:
+                if k != self.parent[v] and v!=self.parent[k]: 
+                    return True
+        return False
 
-
-# %% ../nbs/00_core.ipynb 10
-@patch
-def degree_distribution(self: RGG):
-    dgr=collections.Counter([len(v) for _,v in self.adj.items()])
-    return np.array(list(dgr.items()))
-
-# %% ../nbs/00_core.ipynb 13
-@patch
-def cyclic(self:RGG):
-    for k, lst in self.adj.items():
-        for v in lst:
-            if k != self.parent[v] and v!=self.parent[k]: 
-                return True
-    return False
-
-# %% ../nbs/00_core.ipynb 15
-@patch
-def n_tri(self:RGG):
-    A = (self.distance_matrix<=self.r).astype(np.int64)
-    np.fill_diagonal(A,0)
-    return np.trace(np.linalg.matrix_power(A,3))//6
+    def n_tri(self):
+        A = (self.distance_matrix<=self.r).astype(np.int64)
+        np.fill_diagonal(A,0)
+        return np.trace(np.linalg.matrix_power(A,3))//6
